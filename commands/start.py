1f8df8bb6ea1dfe54b0298dab14c3fd830e8c811
kitvic12 <kitvic12@gmail.com>
2025-05-14 22:47:10 +0300

    Second try

--- commands/start.py
+++ commands/start.py
@@ -6,5 +6,5 @@ from user_activity import activity_tracker
 @bot.message_handler(commands=["start"])
 def start_cmd(message):
     activity_tracker.update_activity(message.from_user, 'command')
-    bot.send_message(message.chat.id, f"Здравствуйте @{message.from_user.username}! Мы рады приветствовать вас в боте, который помогает в отслежке появлений серебристых облаков! Для более подробной информации о способностях нашего бота напишите /help. У нас есть [свой канал](https://t.me/kitvic_nlc_chanel), там вы можете узнавать актуальную информацию о всех изменениях бота и задавать впорпосы на прямую в коментариях!", parse_mode='Markdown')
+    bot.send_message(message.chat.id, f"Здравствуйте @{message.from_user.username}! Мы рады приветствовать вас в боте, который помогает в отслежевании появлений серебристых облаков! Для более подробной информации о способностях нашего бота напишите /help. У нас есть [свой канал](https://t.me/kitvic_nlc_chanel), там вы можете узнавать актуальную информацию о всех изменениях бота и задавать впорпосы на прямую в комментариях!", parse_mode='Markdown')
     log(f"{message.from_user.username} wrote start")
